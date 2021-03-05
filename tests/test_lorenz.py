dcb22267ad061024f4947ddc13b3bf4e5af5907e
Samuel López <44242803+SamShelby@users.noreply.github.com>
2021-03-05 10:33:42 -0600

    plot lorenz in class

--- tests/test_lorenz.py
+++ tests/test_lorenz.py
@@ -5,13 +5,15 @@ def test_construction(lorenz_sys):
     assert lorenz_sys.sigma == 10
     assert lorenz_sys.rho == 28
     assert lorenz_sys.beta == 8. / 3.
-    assert lorenz_sys.initial_state == [0.1, 0, 0]
+    assert lorenz_sys.initial_state == [0, 1, 1.05]
+
 
 def test_solution(lorenz_sys):
     start_time = 0
     end_time = 100
     time_points = np.linspace(start_time, end_time, end_time * 100)
-    x, y, z = lorenz_sys.solve(time_points)
-    assert len(x) == len(time_points)
-    assert len(y) == len(time_points)
-    assert len(z) == len(time_points)
+    lorenz_sys.solve(time_points)
+    xyz = lorenz_sys.get_time_series()
+    assert len(xyz[0]) == len(time_points)
+    assert len(xyz[1]) == len(time_points)
+    assert len(xyz[2]) == len(time_points)
